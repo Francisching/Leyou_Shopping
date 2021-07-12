7aef9b81aaccc035c6704e15275bd37c624e2ae1
taft <tahao@itcast.cn>
2021-07-12 15:55:05 +0800

    第一天项目搭建

---
+++ leyou/ly-common/src/main/java/com/leyou/common/advice/ControllerExceptionAdvice.java
@@ -0,0 +1,20 @@
+package com.leyou.common.advice;
+
+import com.leyou.common.exception.LyException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+//相当于给对应的controller加了try
+@ControllerAdvice
+@Slf4j
+public class ControllerExceptionAdvice {
+
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(LyException ex) {
+
+        return ResponseEntity.status(ex.getStatus()).body(ex.getMessage());
+    }
+}
