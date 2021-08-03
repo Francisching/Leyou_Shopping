92c1628beccbc6164820ee275f42a2740b506405
taft <tahao@itcast.cn>
2021-08-03 18:16:33 +0800

    Revert "第15天分布式事务和微信支付"
    This reverts commit 76affcc89aa548723959cfb6a1b27b40a771d389.

--- demo/seata-demo/order-service/src/main/java/cn/itcast/order/mapper/OrderMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.order.mapper;
-
-import cn.itcast.order.entity.Order;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-
-public interface OrderMapper extends BaseMapper<Order> {
-}
--- leyou/ly-trade/src/main/java/com/leyou/trade/service/PayService.java
+++
@@ -1,9 +0,0 @@
-package com.leyou.trade.service;
-
-import java.util.Map;
-
-public interface PayService {
-    String getPayUrl(Long orderId);
-
-    void payNotify(Map<String, String> paramMap);
-}
